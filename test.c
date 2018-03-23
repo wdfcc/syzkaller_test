b48c393c306b6b2de67bce3187678fa9252a8004
customer <customer@company.com>
2018-03-23 10:21:55 +0800

    fix

--- test.c
+++ test.c
@@ -1,23 +1,24 @@
 #include<linux/init.h>
 #include<linux/module.h>
 #include<linux/fs.h>
-static int test_dev_major=-1;
+#define DEV_NAME "topsec"
+static int major=-1;
 static long test_dev_ioctl (struct file* filep,unsigned int cmd ,unsigned long arg)
 {
 
 	return 0;
 }
 
-static file_operations test_ops
+static struct file_operations test_ops=
 {
-	.owner=THIS_MODULE;
-	.unlocked_ioctl=test_dev_ioctl;
-	.compat_ioctl=test_dev_ioctl;
+	.owner=THIS_MODULE,
+	.unlocked_ioctl=test_dev_ioctl,
+	.compat_ioctl=test_dev_ioctl
 };
 static int __init test_init(void)
 {
-	test_dev_major=register_chardev(0,"testdev",&test_ops);
-	if(test_dev_major<0)
+	major=register_chrdev(0,DEV_NAME,&test_ops);
+	if(major<0)
 	{
 		printk("[*]register error\n");
 	}
@@ -27,7 +28,7 @@ static int __init test_init(void)
 static void __exit test_exit(void)
 {
 
-	unregister_chardev(test_dev_major,"testdev");
+	unregister_chrdev(major,DEV_NAME);
 	printk("[*]unregister_chardev\n");
 }
 module_init(test_init);
